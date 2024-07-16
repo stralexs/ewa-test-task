adaa247cd68b43fccdea614d608a97e67e280525
Alexander <alexandersivko.swift@gmail.com>
2024-07-16 10:39:05 +0300

    Initial Commit

---
+++ EwaTestTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EwaTestTask
+//
+//  Created by Alexander Sivko on 16.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EwaTestTask/EwaTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EwaTestTaskApp.swift
+//  EwaTestTask
+//
+//  Created by Alexander Sivko on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct EwaTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
